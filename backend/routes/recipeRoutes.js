26e40e57528e87d66130ede230eefcd84e452908
Ede Success <edesuccess54@gmail.com>
2022-10-18 23:23:26 +0100

    added urlencoded

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -9,6 +9,6 @@ const urlencodedparser = bodyParser.urlencoded({extended: false})
 router.get("/recipes", getRecipes)
 router.get("/recipes/:id", getSingleRecipe)
 router.delete("/recipes/:id", deleteRecipe)
-router.post("/create", createRecipe)
+router.post("/create",urlencodedparser, createRecipe)
 
 module.exports = router
